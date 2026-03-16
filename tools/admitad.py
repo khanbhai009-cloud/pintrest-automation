a1b02eeddefe438d311fded5f927d94b988c09a1
finisher <ksksysy540@gmail.com>
2026-03-16 15:02:20 +0530

    fix: Admitad domain rzekl.com

--- tools/admitad.py
+++ tools/admitad.py
@@ -3,7 +3,7 @@ from urllib.parse import quote
 from config import ADMITAD_CAMPAIGN_CODE
 
 logger = logging.getLogger(__name__)
-DEEPLINK_BASE = "https://alitems.com/g/{code}/?i=5&ulp={encoded_url}"
+DEEPLINK_BASE = "https://rzekl.com/g/{code}/?i=5&ulp={encoded_url}"
 
 def make_affiliate_link(product_url: str) -> str:
     if not ADMITAD_CAMPAIGN_CODE:
