86e541685175b8a45481ad5001db242deddcfbac
Finisher <ksksysy540@users.noreply.huggingface.co>
2026-04-16 20:49:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -490,4 +490,4 @@ async def cmo_chat_endpoint(req: ChatMessage):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=7860)
